ffcd1def2e868fa0bc643dfb9989aa7da72ec15d
blasc147 <blasc147@gmail.com>
2019-08-15 12:10:12 -0300

    ReactDom.render

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const element = document.createElement('h1');
+// element.innerText = 'Info 2019';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const container = document.getElementById('app');
+
+// container.appendChild(element);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Info 2019 !</h1>;
+
+const container = document.getElementById('app');
+
+// ReactDOM.render(__qué__, __dónde__);
+ReactDOM.render(element, container);
+
+//ReactDOM.render(<App />, document.getElementById('app'));
+//serviceWorker.unregister();
