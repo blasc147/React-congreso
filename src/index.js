a6b7e9768ac8e3b441ecc3fcc2bc5b6a2056693d
blasc147 <blasc147@gmail.com>
2019-08-15 19:58:08 -0300

    Create element y JSX

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-// const element = document.createElement('h1');
-// element.innerText = 'Info 2019';
+// const jsx = <h1>Info 2019</h1>;
+// const element = React.createElement(
+//   'a',
+//   { href: 'https://google.com' },
+//   'Ir a Google'
+// );
+// const name = 'Info';
+// const sum = () => 3 + 3;
+// const element = React.createElement('h1', {}, `Hola, soy ${name}`);
+// const jsx = <h1>Hola soy, {undefined}</h1>;
 
-// const container = document.getElementById('app');
+const jsx = (
+  <div>
+    <h1>Informatorio 2019</h1>
+    <p>Curso de React.js</p>
+  </div>
+);
 
-// container.appendChild(element);
-
-const element = <h1>Info 2019 !</h1>;
-
+const element = React.createElement(
+  'div',
+  {},
+  React.createElement('h1', {}, 'Informatorio 2019'),
+  React.createElement('p', {}, 'Curso React.js')
+);
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
 ReactDOM.render(element, container);
-
-//ReactDOM.render(<App />, document.getElementById('app'));
-//serviceWorker.unregister();
